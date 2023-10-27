bf699aaf3e0b0947f6357fbcc55518b601a4694e
Adrián <adrian.fernandez@enti.cat>
2023-10-27 18:48:30 +0200

    Creación script caida

---
+++ Assets/Project/Scripts/SlowFallingController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SlowFallingController : MonoBehaviour
+{
+
+    #region SlowFalling
+    private void SlowFalling(InputAction.CallbackContext context)
+    {
+
+    }
+
+
+    #endregion SlowFalling
+}
