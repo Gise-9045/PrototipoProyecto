e8c03473b2f9fc8babb9f0a9e614f9df663350c5
Adrián <adrian.fernandez@enti.cat>
2023-10-30 20:05:35 +0100

    Enemy Movement

--- Assets/Project/Scripts/EnemyStats.cs
+++ Assets/Project/Scripts/EnemyStats.cs
@@ -5,23 +5,61 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health = 50;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private int direction = 1;
+
+    [SerializeField] public Transform pointA;
+    [SerializeField] public Transform pointB;
+    [SerializeField] SpriteRenderer sr;
+
+    //Posición actual del Enemigo
+    private Transform currentPoint;
+
+    private void Start()
     {
-        
+        currentPoint = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(health <= 0)
+        if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+    private void FixedUpdate()
+    {
+        
+        if (currentPoint.position.x < pointA.position.x)
+        {
+            direction = 1;
+            sr.flipX = false;
+        }
+        else if(currentPoint.position.x > pointB.position.x)
+        {
+            direction = -1;
+            sr.flipX = true;
+        }
+
+        Move();
+
+    }
+    private void Move()
+    {
+        rb.velocity = new Vector2(direction * speed, rb.velocity.y);
     }
     public void TakeDamage(int damage)
     {
         health -= damage;
         Debug.Log("Damage recieved");
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
     }
 }
